33c76a4ecd28819f7f01d6ec0a2f8ec976143af5
akbarshah19 <akbarshah19@icloud.com>
2025-01-26 23:48:16 +0500

    Final commit

--- DocumentScanner/Views/DocumentDetailView.swift
+++ DocumentScanner/Views/DocumentDetailView.swift
@@ -7,12 +7,21 @@
 
 import SwiftUI
 import PDFKit
+import LocalAuthentication
 
 struct DocumentDetailView: View {
     
     var document: Document
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) private var modelContext
+    @Environment(\.scenePhase) private var scenePhase
+    
+    @State private var isLoading: Bool = false
+    @State private var showFileMover: Bool = false
+    @State private var fileURL: URL?
+    
+    @State private var isLockAvailable: Bool?
+    @State private var isUnlocked: Bool = false
     
     var body: some View {
         if let pages = document.pages?.sorted(by: { $0.pageIndex < $1.pageIndex }) {
@@ -35,6 +44,31 @@ struct DocumentDetailView: View {
             }
             .background(.black)
             .toolbarVisibility(.hidden, for: .navigationBar)
+            .loadingScreen(status: $isLoading)
+            .overlay {
+                LockView()
+            }
+            .fileMover(isPresented: $showFileMover, file: fileURL) { result in
+                if case .failure(let failure) = result {
+                    guard let fileURL else { return }
+                    try? FileManager.default.removeItem(at: fileURL)
+                    self.fileURL = nil
+                }
+            }
+            .onAppear {
+                guard document.isLocked else {
+                    isUnlocked = true
+                    return
+                }
+                
+                let context = LAContext()
+                isLockAvailable = context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+            }
+            .onChange(of: scenePhase) { oldValue, newValue in
+                if newValue != .active && document.isLocked {
+                    isUnlocked = false
+                }
+            }
         }
     }
     
@@ -46,7 +80,9 @@ struct DocumentDetailView: View {
             .hSpacing(.center)
             .overlay(alignment: .trailing) {
                 Button {
-                    //
+                    document.isLocked.toggle()
+                    isUnlocked = !document.isLocked
+                    try? modelContext.save()
                 } label: {
                     Image(systemName: document.isLocked ? "lock.fill" : "lock.open.fill")
                 }
@@ -81,25 +117,78 @@ struct DocumentDetailView: View {
         .padding([.horizontal, .top], 15)
     }
     
+    @ViewBuilder
+    private func LockView() -> some View {
+        if document.isLocked {
+            ZStack {
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 6) {
+                    if let isLockAvailable, !isLockAvailable {
+                        Text("Please enable biometric access in Settings to unlock this document!")
+                            .multilineTextAlignment(.center)
+                            .frame(width: 200)
+                    } else {
+                        Image(systemName: "lock.fill")
+                            .font(.largeTitle)
+                        
+                        Text("Tap to unlock!")
+                            .font(.callout)
+                    }
+                }
+                .padding(15)
+                .background(.bar, in: .rect(cornerRadius: 10))
+                .contentShape(.rect)
+                .onTapGesture(perform: authenticateUser)
+                .opacity(isUnlocked ? 0 : 1)
+                .animation(snappy, value: isUnlocked)
+            }
+        }
+    }
+    
+    private func authenticateUser() {
+        let context = LAContext()
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Locked Document") { status, _ in
+                DispatchQueue.main.async {
+                    self.isUnlocked = status
+                }
+            }
+        } else {
+            isLockAvailable = false
+            isUnlocked = false
+        }
+    }
+    
     private func createAndShareDocument() {
         guard let pages = document.pages?.sorted(by: { $0.pageIndex < $1.pageIndex }) else {
             return
         }
         
-        let pdfDoc = PDFDocument()
-        for index in pages.indices {
-            if let pageImage = UIImage(data: pages[index].pageData),
-               let pdfPage = PDFPage(image: pageImage) {
-                pdfDoc.insert(pdfPage, at: index)
+        isLoading = true
+        
+        Task.detached(priority: .high) { [document] in
+            let pdfDoc = PDFDocument()
+            for index in pages.indices {
+                if let pageImage = UIImage(data: pages[index].pageData),
+                   let pdfPage = PDFPage(image: pageImage) {
+                    pdfDoc.insert(pdfPage, at: index)
+                }
             }
-        }
-        
-        var pdfURL = FileManager.default.temporaryDirectory
-        let fileName = "\(document.name).pdf"
-        pdfURL.append(path: fileName)
-        
-        if pdfDoc.write(to: pdfURL) {
             
+            var pdfURL = FileManager.default.temporaryDirectory
+            let fileName = "\(document.name).pdf"
+            pdfURL.append(path: fileName)
+            
+            if pdfDoc.write(to: pdfURL) {
+                await MainActor.run { [pdfURL] in
+                    fileURL = pdfURL
+                    showFileMover = true
+                    isLoading = false
+                }
+            }
         }
     }
 }
